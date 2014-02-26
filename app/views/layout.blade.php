27733f46c85b675e76ddee93ed5f83cfe5e83639
Jake Shelby <jake.shelby@me.com>
2014-02-26 10:57:48 -0700

    Fixed comment bug

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     <title>Laravel Authentication Demo</title>
-    <?php // {{ HTML::style('/css/style.css') }} ?>
+    <?php /* {{ HTML::style('/css/style.css') }} */ ?>
 </head>
 <body>
     <div id="container">
