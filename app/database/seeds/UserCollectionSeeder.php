92a6381e7ea787d67a5cb594b686a1caa48cbb24
Jake Shelby <jake.shelby@me.com>
2014-03-04 23:29:10 -0700

    Added more users to the db seeder

--- app/database/seeds/UserCollectionSeeder.php
+++ app/database/seeds/UserCollectionSeeder.php
@@ -34,8 +34,43 @@ class UserCollectionSeeder extends Seeder {
 			'username' => 'jcoats',
 			'password' => 'joshua$',
 		),
+		array(
+			'username' => 'poophead',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'jackson',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'flackson',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'jetson',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'mackelroy',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'davidsons',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'rappaport',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'your_mom',
+			'password' => 'nothing',
+		),
+		array(
+			'username' => 'the_cook',
+			'password' => 'nothing',
+		),
 	);
-
 
 	/**
 	 * Run the database seeds.
